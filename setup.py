b09d85339bf96c57f870acd3f24805bf427161b9
osarwar <owais.sarwar@gmail.com>
2020-05-27 13:44:35 -0500

    setup package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rFS", 
+    version="0.1",
+    author="Owais Sarwar",
+    author_email="osarwar@andrew.cmu.edu",
+    description="A tool to build regression models using Regularized Forward Selection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/osarwar/rFS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    'numpy>=1.16.1', 
+    'rpy2>=3.3.3'],
+)
